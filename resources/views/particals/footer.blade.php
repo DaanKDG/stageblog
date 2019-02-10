64c8640be0c1dae59e076724ec5ade4fcd185811
daankdg <daan.boschmans@student.kdg.be>
2019-02-10 14:19:23 +0100

    initial commit

---
+++ resources/views/particals/footer.blade.php
@@ -0,0 +1,12 @@
+<footer id="footer" class="footer" style='background-color: #4392F1'>
+    <div class="container text-center">
+        <div class="row content">
+            <div class="col-md-4 offset-md-4">
+                <div class="mt-4">
+                    <h3 style='font-weight: bold;'>Laravel & Vue</h3>
+                    <small>Daan Boschmans</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/particals/jumbotron.blade.php
@@ -0,0 +1,10 @@
+<div class="jumbotron jumbotron-fluid" style='background-color: #4392F1;'>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-10 offset-md-1">
+                <h1 style='font-weight: bold;'>Take The Lead</h1>
+                <p>Daan Boschmans</p>
+            </div>
+        </div>
+    </div>
+</div>
